10a28949535ac3cc8ad8ceb2a427fbe47381c8af
Bassil <bassilede88@gmail.com>
2023-01-22 00:13:49 +0100

    Updates

--- index.ccf7259c.js
+++ index.ccf7259c.js
@@ -1,2 +1,2 @@
 const dark_mode_button=document.getElementById("dark_mode_button");dark_mode_button.addEventListener("click",(()=>{document.body.classList.toggle("dark_mode")}));const buttons=document.querySelectorAll("button");buttons.forEach((t=>{t.addEventListener("click",(()=>{const e=t.getAttribute("data-state");e&&"closed"!==e?(t.setAttribute("data-state","closed"),t.setAttribute("aria-expanded","false")):(t.setAttribute("data-state","opened"),t.setAttribute("aria-expanded","true"))}))}));
-//# sourceMappingURL=index.d0767250.js.map
+//# sourceMappingURL=index.ccf7259c.js.map
